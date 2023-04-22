f31c6e56e46ef6799d9cb6fd06d219cde34f751d
Jack Lin <jacklin1229@gmail.com>
2023-04-22 02:27:20 -0700

    add flask

--- journify_frontend/src/App.js
+++ journify_frontend/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import APIService from "./component/APIService";
 
 function App() {
+  const [title, setTitle] = useState("THIS IS TITLE");
+  const [body, setBody] = useState("THIS IS BODY");
+
+  //title and body are temp, can be collected from form using useSTate
+  //use APIService component to call backend
+  APIService.InsertArticle({ title, body });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> adawdo
+          rdaddqdadddawdaadwddddadwdwawaddadweload.
         </p>
         <a
           className="App-link"
