dbb7e5baca1cd1f032d805468cc4ed44ab9ed9c2
Alan Vuong <alankvuong@gmail.com>
2023-04-22 20:36:16 -0700

    Adding destination page

--- journify_frontend/src/App.js
+++ journify_frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import SignUp from "./pages/SignUp/SignUp.js";
 import Login from "./pages/Login/Login.js";
+import DestinationPage from "./pages/DestinationsPage/DestinationPage"
 
 function App() {
   const [title, setTitle] = useState("THIS IS TITLE");
@@ -22,6 +23,7 @@ function App() {
         <Routes>
           {/* <Route exact path="/" element={<Navbar />} /> */}
           <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/explore" element={<DestinationPage />} />
           <Route exact path="/signup" element={<SignUp />} />
           <Route exact path="/login" element={<Login/>} />
         </Routes>
